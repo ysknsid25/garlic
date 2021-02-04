5290c5738dfef92f2ca3b7ccc893c3336f170f53
Ramen Tabetarou <kobeengineer1225@gmail.com>
2021-02-04 23:33:49 +0900

    finish
    Signed-off-by: Ramen Tabetarou <kobeengineer1225@gmail.com>

--- CalcCalendarData/main.js
+++ CalcCalendarData/main.js
@@ -1,9 +1,22 @@
 const main = () => {
-    getCalendarEvents();
-};
+    const calendarEvents = getCalendarEvents();
+    const inputSheet = getNewSheet();
 
-const CALENDAR_ID = ''; //カレンダーID
-const KEY_WORD = '#hoge';
+    inputSheet.getRange(4, 4).setValue(getYearMonth());
+
+    let nowLine = 12; //12行目からスタート
+    const sumDur = durSum();
+
+    calendarEvents.map((event) => {
+        inputSheet.getRange(nowLine, 3).setValue(event.date);
+        inputSheet.getRange(nowLine, 4).setValue(event.title);
+        inputSheet.getRange(nowLine, 6).setValue(event.beginTime);
+        inputSheet.getRange(nowLine, 7).setValue(event.endTime);
+        inputSheet.getRange(nowLine, 8).setValue(event.dur);
+        inputSheet.getRange(43, 8).setValue(sumDur(event.dur));
+        nowLine++;
+    });
+};
 
 const getCalendarEvents = () => {
 
@@ -14,7 +27,7 @@ const getCalendarEvents = () => {
     const startTime = new Date(year + '/' + month + '/1 00:00:00');
     const endTime = new Date(year + '/' + month + '/31 00:00:00');
 
-    calendar
+    return calendar
     .getEvents(startTime, endTime)
     .filter((event) => event.getTitle().indexOf(KEY_WORD) > -1)
     .map(
@@ -22,21 +35,34 @@ const getCalendarEvents = () => {
         (
             new Object(
                 {
-                    title: event.getTitle(),
+                    title: event.getTitle().replace(KEY_WORD, "").trim(),
                     date: (
                             (event) => {
                                 const date = new Date(event.getStartTime());
                                 return date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
                             }
                             )(event),
-                    dur: convMillisecondToMinute(event.getEndTime() - event.getStartTime()),
+                    dur: convMillisecondToHour(event.getEndTime() - event.getStartTime()),
+                    beginTime: getTime(event.getStartTime()),
+                    endTime: getTime(event.getEndTime()),
                     description: event.getDescription()
                 }
             )
         )
     );
-    //console.log(event.getTitle() + event.getStartTime() + event.getEndTime() + event.getDescription()
 
+};
+
+const getTime = (dateTime) => {
+    const date = new Date(dateTime);
+    return complementZero(date.getHours()) + ':' + complementZero(date.getMinutes());
+};
+
+const complementZero = (time) => {
+    if(0 <= time && time <= 9){
+        return '0' + time;
+    }
+    return time;
 };
 
 const durSum = () => {
@@ -45,7 +71,7 @@ const durSum = () => {
         return sum += dur;
     };
     return add;
-}
+};
 
 const getTargetMonth = (month) => {
     if(month == 0){
@@ -61,4 +87,19 @@ const getTargetYear = (year, month) => {
     return year;
 };
 
-const convMillisecondToMinute = (milliSecond) => milliSecond / 60000;
+const convMillisecondToHour = (milliSecond) => milliSecond / 3600000;
+
+const getNewSheet = () => {
+    const DRIVEINFO = getGoogleDriveInfo(); 
+    const spreadsheet = SpreadsheetApp.openByUrl(DRIVEINFO.SP_URL);
+    const newSheet = spreadsheet.getSheetByName(DRIVEINFO.ORIGINAL_SHEET_NM).copyTo(spreadsheet);
+    newSheet.setName(getYearMonth());
+    spreadsheet.setActiveSheet(newSheet);
+    spreadsheet.moveActiveSheet(1);
+    return newSheet;
+};
+
+const getYearMonth = () => {
+    const dateObj = new Date();
+    return dateObj.getFullYear() + '年' + getTargetMonth(dateObj.getMonth()) + '月度';
+};
